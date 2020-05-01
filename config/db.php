430ef9f75d1b921294b6538fdad8a8b423c58a43
shalamnik <shalamberidze00@bk.ru>
2020-05-01 16:04:13 +0300

    added defaultRoute

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii_db',
     'username' => 'root',
-    'password' => 'password',
+    'password' => '131619121518nM$',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
--- views/users/index.php
+++ views/users/index.php
@@ -7,7 +7,7 @@ use yii\grid\GridView;
 /* @var $searchModel app\models\UsersSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Users';
+$this->title = 'Payment Users';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="users-index">
@@ -15,7 +15,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Create Users', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Create User', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
