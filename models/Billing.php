30557a53ce5fa0e44a902701b516c70a43f8db91
shalamnik <shalamberidze00@bk.ru>
2020-05-01 09:13:15 +0300

    update Billing model by Gii

--- models/Billing.php
+++ models/Billing.php
@@ -1,15 +1,63 @@
-<?php 
+<?php
 
 namespace app\models;
 
-use yii\db\ActiveRecord;
+use Yii;
 
-class Billing extends ActiveRecord
+/**
+ * This is the model class for table "billing".
+ *
+ * @property int $id
+ * @property int|null $user_id
+ * @property int|null $payment
+ * @property string $created_at
+ * @property string|null $updated_at
+ *
+ * @property Users $user
+ */
+class Billing extends \yii\db\ActiveRecord
 {
-    public function getUsers()
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
     {
-        $this->hasMany(Users::className(), ['id' => 'user_id']);
+        return 'billing';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'payment'], 'integer'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => Users::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'payment' => 'Payment',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(Users::className(), ['id' => 'user_id']);
     }
 }
-
-?>
