0db013e1c7811c3cfb858dddf2f8006308c5181c
shalamnik <shalamberidze00@bk.ru>
2020-05-01 09:11:00 +0300

    update Users model by Gii

--- models/Users.php
+++ models/Users.php
@@ -1,15 +1,61 @@
-<?php 
+<?php
 
 namespace app\models;
 
-use yii\db\ActiveRecord;
+use Yii;
 
-class Users extends ActiveRecord
+/**
+ * This is the model class for table "users".
+ *
+ * @property int $id
+ * @property string $firstname
+ * @property int $age
+ * @property int|null $phone
+ *
+ * @property Billing[] $billings
+ */
+class Users extends \yii\db\ActiveRecord
 {
-    public function getBilling()
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'users';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['firstname', 'age'], 'required'],
+            [['age', 'phone'], 'integer'],
+            [['firstname'], 'string', 'max' => 20],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'firstname' => 'Firstname',
+            'age' => 'Age',
+            'phone' => 'Phone',
+        ];
+    }
+
+    /**
+     * Gets query for [[Billings]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBillings()
     {
         return $this->hasMany(Billing::className(), ['user_id' => 'id']);
     }
 }
-
-?>
